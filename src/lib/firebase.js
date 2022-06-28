dc2b75fb51e0e40af9a00a63d42b666a657a2dbb
DucHieu <duchieu20002501@gmail.com>
2022-06-28 20:57:39 +0700

    fire base

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,20 +9,22 @@ import { initializeApp } from "@firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDYHEP67WxUUZMRJ3NEJrTGIfwHGoVG3qg",
-    authDomain: "fir-sample-a04d1.firebaseapp.com",
-    projectId: "fir-sample-a04d1",
-    storageBucket: "fir-sample-a04d1.appspot.com",
-    messagingSenderId: "3958416278",
-    appId: "1:3958416278:web:bc2b547af6903d682373b7",
+  apiKey: "AIzaSyBcFvjC1haGWchAy9nZ6lIdy9xCUmyhLM8",
+  authDomain: "hieudz-3f9a6.firebaseapp.com",
+  projectId: "hieudz-3f9a6",
+  storageBucket: "hieudz-3f9a6.appspot.com",
+  messagingSenderId: "868387452098",
+  appId: "1:868387452098:web:3671c5b1b95cd94383b75b",
+  measurementId: "G-BZLM3RZDC9"
 };
 const app = initializeApp({
-    apiKey: "AIzaSyDYHEP67WxUUZMRJ3NEJrTGIfwHGoVG3qg",
-    authDomain: "fir-sample-a04d1.firebaseapp.com",
-    projectId: "fir-sample-a04d1",
-    storageBucket: "fir-sample-a04d1.appspot.com",
-    messagingSenderId: "3958416278",
-    appId: "1:3958416278:web:bc2b547af6903d682373b7",
+  apiKey: "AIzaSyBcFvjC1haGWchAy9nZ6lIdy9xCUmyhLM8",
+  authDomain: "hieudz-3f9a6.firebaseapp.com",
+  projectId: "hieudz-3f9a6",
+  storageBucket: "hieudz-3f9a6.appspot.com",
+  messagingSenderId: "868387452098",
+  appId: "1:868387452098:web:3671c5b1b95cd94383b75b",
+  measurementId: "G-BZLM3RZDC9"
 });
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
